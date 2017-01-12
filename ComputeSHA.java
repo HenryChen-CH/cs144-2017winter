d786672565b65a7ca8f0ddec8e23f8f36416c2f8
HenryChen-CH <henry.hao.chen71@gmail.com>
2017-01-12 09:03:50 -0800

    refactor catch exception

--- ComputeSHA.java
+++ ComputeSHA.java
@@ -1,5 +1,5 @@
-import java.security.MessageDigest;
-import java.io.FileInputStream;
+import java.security.*;
+import java.io.*;
 
 class ComputeSHA {
     public static void main(String[] args) {
@@ -11,24 +11,28 @@ class ComputeSHA {
         MessageDigest md;
         try {
             fs = new FileInputStream(args[0]);
-            try {
-                md = MessageDigest.getInstance("SHA-1");
-                byte[] bytes = new byte[1024];
-                int n = 0;
-                while ((n = fs.read(bytes)) != -1) {
-                    md.update(bytes, 0, n);
-                }
-                byte[] mdbytes = md.digest();
-                StringBuffer sb = new StringBuffer();
-                for (byte b: mdbytes) {
-                    sb.append(Integer.toString((b&0xff)+0x100, 16).substring(1));
-                }
-                System.out.println(sb.toString());
-            } catch (Exception e) {
-                System.out.println("MessageDigest error");
+            md = MessageDigest.getInstance("SHA-1");
+            byte[] bytes = new byte[1024];
+            int n = 0;
+            while ((n = fs.read(bytes)) != -1) {
+                md.update(bytes, 0, n);
             }
-        } catch (Exception e) {
-            System.out.println("File error");
+            byte[] mdbytes = md.digest();
+            StringBuffer sb = new StringBuffer();
+            for (byte b: mdbytes) {
+                sb.append(Integer.toString((b&0xff)+0x100, 16).substring(1));
+            }
+            System.out.println(sb.toString());
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        } catch (SecurityException  e) {
+            System.out.println("Access denied!");
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("MessageDigest no such algorithm");
+        } catch (IOException e) {
+            System.out.println("IO error");
+        } finally {
+            System.out.println("Error");
         }
         return;
     }
