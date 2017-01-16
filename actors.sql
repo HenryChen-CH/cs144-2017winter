e3bf3c7ef675596cf8a919ca4c84f273e1163fde
HenryChen-CH <henry.hao.chen71@gmail.com>
2017-01-15 20:18:51 -0800

    clean proj1

--- actors.sql
+++
@@ -1,4 +0,0 @@
-create table Actors (Name VARCHAR(40), Movie VARCHAR(80), Year INTEGER, Role VARCHAR(40));
-load data local infile '~/data/actors.csv' into table Actors FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
-select Name from Actors where Movie='Die Another Day';
-drop table Actors;
