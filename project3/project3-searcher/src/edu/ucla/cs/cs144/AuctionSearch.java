07f483b54a177da2f7447f96ea8b16954a8c1168
HenryChen-CH <henry.hao.chen71@gmail.com>
2017-01-29 12:45:50 -0800

    WIP

--- project3/project3-searcher/src/edu/ucla/cs/cs144/AuctionSearch.java
+++ project3/project3-searcher/src/edu/ucla/cs/cs144/AuctionSearch.java
@@ -2,6 +2,7 @@ package edu.ucla.cs.cs144;
 
 import java.io.IOException;
 import java.io.File;
+import java.io.StringWriter;
 import java.sql.*;
 import java.util.Date;
 import java.util.ArrayList;
@@ -17,6 +18,15 @@ import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.store.FSDirectory;
+
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Element;
+import org.w3c.dom.Attr;
 
 import java.util.HashSet;
 
@@ -39,6 +49,9 @@ public class AuctionSearch implements IAuctionSearch {
 	private IndexSearcher searcher;
 	private QueryParser parser;
 	private static String indexFolder = "/var/lib/lucene/index1/";
+	private static String[] colNames = new String[]
+            {"Name", "Category", "Currently", "Buy_Price", "First_Bid", "Number_of_Bids", "Bids", "Location", "Country",
+                    "Started", "Ends", "Seller","Description"};
 	
 	public SearchResult[] basicSearch(String query, int numResultsToSkip, 
 			int numResultsToReturn) {
@@ -113,6 +126,72 @@ public class AuctionSearch implements IAuctionSearch {
 
 	public String getXMLDataForItemId(String itemId) {
 		// TODO: Your code here!
+        Connection conn;
+        int id = Integer.parseInt(itemId);
+
+        try {
+            conn = DbManager.getConnection(true);
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            org.w3c.dom.Document doc = docBuilder.newDocument();
+
+            PreparedStatement item_stmt = conn.prepareStatement("select * from Items where ItemID=?");
+            PreparedStatement category_stmt = conn.prepareCall("select Category from Categories where ItemID=?");
+            PreparedStatement bids_stmt = conn.prepareStatement("select * from Bids where ItemId=?");
+
+            item_stmt.setInt(1, id);
+            category_stmt.setInt(1, id);
+            ResultSet cat_rs = category_stmt.executeQuery();
+            ResultSet item_rs = item_stmt.executeQuery();
+            Element item = doc.createElement("Item");
+            item.setAttribute("ItemID", itemId);
+            doc.appendChild(item);
+            Element num_bids = doc.createElement("Number_of_Bids");
+            int num = 0;
+            if (item_rs.next()) {
+                for (String col: colNames) {
+                    if (col.equals("Category")) {
+                        while (cat_rs.next()) {
+                            Element cat = doc.createElement(col);
+                            cat.appendChild(doc.createTextNode(cat_rs.getString(col)));
+                            item.appendChild(cat);
+                        }
+                    } else if (col.equals("Currently") || col.equals("Buy_Price")) {
+                        Double price = item_rs.getDouble(col);
+                        if (price != null && price != 0) {
+                            Element tmp = doc.createElement(col);
+                            tmp.appendChild(doc.createTextNode("$" + String.format("%.2f", price)));
+                            item.appendChild(tmp);
+                        }
+                    } else if (col.equals("Number_of_Bids")) {
+                        item.appendChild(num_bids);
+                    } else if (col.equals("Bids")) {
+
+                    }
+//                    } else {
+//                        Element tmp = doc.createElement(col);
+//                        tmp.appendChild(doc.createTextNode(item_rs.getString(col)));
+//                        item.appendChild(tmp);
+//                    }
+                }
+            } else {
+                return "";
+            }
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+            transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            StreamResult results = new StreamResult(new StringWriter());
+            DOMSource source = new DOMSource(doc);
+            transformer.transform(source, results);
+
+            return results.getWriter().toString();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
 		return "";
 	}
 	
