88940a4a380754d4459f7c7ddec5aa682f0cfc09
HenryChen-CH <henry.hao.chen71@gmail.com>
2017-02-11 10:55:50 -0800

    complete ProxyServlet

--- project4/src/edu/ucla/cs/cs144/ProxyServlet.java
+++ project4/src/edu/ucla/cs/cs144/ProxyServlet.java
@@ -1,12 +1,16 @@
 package edu.ucla.cs.cs144;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.HttpURLConnection;
+import java.net.URL;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class ProxyServlet extends HttpServlet implements Servlet {
        
@@ -15,5 +19,16 @@ public class ProxyServlet extends HttpServlet implements Servlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         // your codes here
+        URL url = new URL("http://google.com/complete/search?output=toolbar&q="+request.getAttribute("q"));
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.connect();
+        PrintWriter out = response.getWriter();
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line;
+        while ((line = in.readLine()) != null) {
+            out.write(line);
+        }
+        in.close();
+        out.close();
     }
 }
