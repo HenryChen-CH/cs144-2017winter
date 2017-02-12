2cb7da050dca17644d3ff5e3f7dd08a7e67936e2
HenryChen-CH <henry.hao.chen71@gmail.com>
2017-02-11 16:59:17 -0800

    encode query string and set content type to text/xml, so XMLHttpRequest.responseXML is not null

--- project4/src/edu/ucla/cs/cs144/ProxyServlet.java
+++ project4/src/edu/ucla/cs/cs144/ProxyServlet.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.URLEncoder;
 
 public class ProxyServlet extends HttpServlet implements Servlet {
        
@@ -19,9 +20,10 @@ public class ProxyServlet extends HttpServlet implements Servlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         // your codes here
-        URL url = new URL("http://google.com/complete/search?output=toolbar&q="+request.getAttribute("q"));
+        URL url = new URL("http://google.com/complete/search?output=toolbar&q="+URLEncoder.encode((String)request.getParameter("q"), "UTF-8"));
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.connect();
+        response.setContentType("text/xml");
         PrintWriter out = response.getWriter();
         BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
         String line;
